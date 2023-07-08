0dde00d7aec5cf2d9721dd4b2d12d09ee0e53097
aman anurag <aman.anurag07>
2023-07-08 16:31:27 +0530

    commiting build to deploy to github pages

---
+++ build/fav-intro.js
@@ -0,0 +1,22 @@
+class Aman  {
+    constructor() {
+        this.name = "Aman Anurag";
+        this.location = "India";
+        this.occupation = "Software Engineer";
+        
+        }
+        workExperience() {
+            return [
+                {'2021:now' : 'Solution Architect  at IBM India Pvt. Ltd.'},
+                {'2020:2021' : 'Consutant Engineer  at Xebia India Pvt. Ltd.'},
+                {'2018:2020' : 'Seniour Software Engineer  at Capgemini India Pvt. Ltd.'},
+                {'2015:2018' : 'Software Engineer  at Tata Consultancy India Pvt. Ltd.'}
+            ]
+        }
+        skills() {
+            return [
+                'Javascript','Nodejs', 'Reactjs', 'Angular', 'Next.js', 'Java', 'Backbase',
+                'HTML', 'CSS', 'Spring Boot', 'Kubernetes', 'Docker', 'Python'
+            ]
+        }
+    }
