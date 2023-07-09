f1f759cd75b47d34e1f5aa1c32f697bc6d0ed304
aman anurag <aman.anurag07>
2023-07-09 12:17:33 +0530

    setState hook example added

--- src/component/login/login-component.js
+++ src/component/login/login-component.js
@@ -1,14 +1,41 @@
 import './login.css'
 import Example from '../example/example-component';
 import Avatar from '../avatar/avatar-component';
+import { useState } from 'react';
 function Login(){
+
+    const [name, setName  ] = useState('');
+    const [password, setPassword  ] = useState('');
+    function submitHandler(e) {
+        e.preventDefault();
+        setName('');
+        setPassword('');
+        
+    }
+
+    const onChangeHandler = (field) => {
+        
+        
+        if(field === 'username'){
+         
+            return (e) => {
+                setName(e.target.value);
+            }
+        }else{
+        
+             return (e) => {
+                setPassword(e.target.value);
+             }
+        }
+        
+    }
     return (
         <>
         <Avatar/>
          <div class="login">
-            <input type="text" placeholder="Enter your username" />
-            <input type="password" placeholder="Enter your password" />
-            <input type="submit" value="Login" />
+            <input type="text" placeholder="Enter your username" value={name} onChange={onChangeHandler('username')}/>
+            <input type="password" placeholder="Enter your password" value={password} onChange={onChangeHandler('password')} />
+            <input type="submit" value="Login" onClick={(e) => submitHandler(e)} />
             </div>
         
         
